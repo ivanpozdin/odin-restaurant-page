df8c6cb0daed79fe1b8380b4064074dfe5bcd006
ivanpozdin <ivanllpozdin@gmail.com>
2023-09-13 02:16:35 +0400

    Fix city name in home page to match contact page

--- src/render-home-content.js
+++ src/render-home-content.js
@@ -2,7 +2,7 @@ export default function (element) {
   const homeHTML = `
   <div id="home-restaurant-info-container">
   <p>
-  POZDINNI is the best totally fictional pasta restaurant in Chicago. We've been cooking delicious italian dishes in website's author's imagination since 1977. This non-existent restaurant proved its food taste and quality by generations of our customers, number of which is suspiciously close to zero.
+  POZDINNI is the best totally fictional pasta restaurant in Seattle. We've been cooking delicious italian dishes in website's author's imagination since 1977. This non-existent restaurant proved its food taste and quality by generations of our customers, number of which is suspiciously close to zero.
   </p>
   </div>
     `;
